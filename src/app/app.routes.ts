f0576119881fd419d6fc7a1a62ee2ebad10644b9
fleurkernevez <fleur@transparence.pro>
2025-06-28 16:03:58 +0200

    creation of the page to submit a new feedback - installation of Material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { FeedbackComponent } from './pages/feedback/feedback/feedback.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent}
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'feedback', component: FeedbackComponent },
+
+  // Route wildcard pour gérer les URLs non trouvées
+  { path: '**', redirectTo: 'home' }
 ];
