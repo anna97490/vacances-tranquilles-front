593600066058964794e6c90d7230441f772ba4bc
fleurkernevez <fleur@transparence.pro>
2025-07-02 15:09:56 +0200

    creation of the visual of the component for the service search to be finalized

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { FeedbackComponent } from './pages/feedback/feedback/feedback.component';
+import { ServiceSearchComponent } from './pages/service-search/service-search/service-search.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'feedback', component: FeedbackComponent },
+  { path: 'service-search', component: ServiceSearchComponent },
 
   // Route wildcard pour gérer les URLs non trouvées
   { path: '**', redirectTo: 'home' }
