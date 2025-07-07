48f0f12fb56837a576ca0e79ce26e3b17d5a4d11
marinaPerezYnov <148948211+marinaPerezYnov@users.noreply.github.com>
2025-07-07 13:48:41 +0200

    update without errors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { TermsAndConditionsComponent } from './components/terms-and-conditions/terms-and-conditions.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -18,28 +17,6 @@ export const routes: Routes = [
         loadComponent: () => import('./components/terms-and-conditions/terms-and-conditions.component')
           .then(m => m.TermsAndConditionsComponent),
         data: { isCGV: true } // Indique que c'est pour les CGV  
-      }
-    ]
-  },
-  
-  // Routes d'authentification avec paramètres pour le type d'utilisateur
-  { 
-    path: 'auth',
-    // :userType pour distinguer entre "particulier" et "prestataire"
-    children: [
-      { 
-        path: 'login/:userType', 
-        loadComponent: () => import('./components/auth-form/auth-form.component')
-          .then(m => m.AuthFormComponent),
-        //   Utilisé la propriété data pour passer des informations supplémentaires aux composants chargés (comme isRegister)
-        data: { isRegister: false }
-      },
-      { 
-        path: 'register/:userType', 
-        loadComponent: () => import('./components/register-form/register-form.component')
-          .then(m => m.RegisterFormComponent),
-        //   Utilisé la propriété data pour passer des informations supplémentaires aux composants chargés (comme isRegister)
-        data: { isRegister: true }
       }
     ]
   },
