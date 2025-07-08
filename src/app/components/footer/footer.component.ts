e163db40377e2acf76afa2d02b6b4789311bb786
marina.perez@ynov.com <marina.perez@ynov.com>
2025-07-08 14:41:49 +0200

    add new pages and components

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatDividerModule,
+    MatListModule
+  ],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+
+}
