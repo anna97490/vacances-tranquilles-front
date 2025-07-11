3c2fa473cd5426b81ab80422ff336d16b539d524
marina.perez@ynov.com <marina.perez@ynov.com>
2025-07-11 14:17:25 +0200

    fix: gestion de la visibilité du header selon l'authentification et affichage dynamique des icônes
    - Le header n'est plus affiché tant que l'utilisateur n'est pas authentifié
    - Les icônes du header changent au survol des liens de navigation
    - Les icônes du header reflètent également la page active selon l'URL

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,33 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, 
-    ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
+  imports: [
+    RouterOutlet, 
+    HeaderComponent, 
+    ReactiveFormsModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatButtonModule, 
+    MatIconModule, 
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'frontend';
+  isConnected = false;
+
+  constructor() {
+    if (localStorage.getItem('token')) {
+      this.isConnected = false;
+    } else {
+      this.isConnected = true;
+    }
+  }
 }
