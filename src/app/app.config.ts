534b4acd518dc5ae782a8211d30f374023251097
fleurkernevez <fleur@transparence.pro>
2025-08-07 10:43:08 +0200

    ts doc automation and app.config correction

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { ConfigService } from './services/config/config.service';
